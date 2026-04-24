b261f2c3c8a567c9dcf8285f48d59c652fab66d6
anna <anna.guccione@studenti.units.it>
2026-04-24 23:01:46 +0200

    add testing file for timing

---
+++ code/grsc_cb/testing.py
@@ -0,0 +1,91 @@
+import cProfile, pstats
+from grsc_cb_model import GRSC_CB_Model
+from grsc_cb_instance import GRSC_CB_Instance
+import numpy as np
+from scipy.spatial import Delaunay, ConvexHull
+import networkx as nx
+
+SEED = 15
+
+np.random.seed(SEED)
+# number of land parcels
+n = 900
+# number of species
+m = 10
+# number of max connected areas
+k = 1
+# range of habitat suitability function
+w_min, w_max = 20, 100
+# range of costs
+c_min, c_max = 1, 100
+# lambda percentage threshold
+lambda_threshold = 0.05
+# buffer size
+d = 1
+# tau
+tau = 0.5
+
+
+# generate a random instance Delunay graph
+points = np.random.rand(n, 2)
+tri = Delaunay(points)
+
+G = nx.Graph()
+G.add_nodes_from(range(len(points)))
+
+# for each node, it connects it to its neighbors using Delaunay triangulation
+for simplex in tri.simplices:
+    i, j, h = simplex
+    G.add_edge(i, j)
+    G.add_edge(j, h)
+    G.add_edge(i, h)
+
+# determines the set of external nodes  
+external_nodes = set(ConvexHull(points).vertices)
+nx.draw(G, points, with_labels=True)
+print(f"External nodes: {[int(i) for i in external_nodes]}")
+
+# costs of land parcels
+c = {i: np.random.randint(c_min, c_max + 1) for i in G.nodes}
+
+# specie definition
+S_1 = list(range(m//3))
+S_2 = list(range(m//3, m))
+P_1 = len(S_1)
+P_2 = len(S_2)
+
+# habitat suitability score function
+w = {}
+for s in S_1 + S_2:
+    prob_zero = 0.2 if s in S_1 else 0.1
+    for i in G.nodes:
+        if i in external_nodes and s in S_1:
+            w[(i, s)] = 0
+        elif np.random.rand() < prob_zero:
+            w[(i, s)] = 0
+        else:
+            w[(i, s)] = np.random.randint(w_min, w_max + 1)
+            
+# suitability quota
+lambda_s = {s: lambda_threshold * sum(w[(i, s)] for i in G.nodes) for s in S_1 + S_2}
+
+# toy instance
+V, E = set(G.nodes), set(G.edges)
+instance = GRSC_CB_Instance(V, E, points, 
+                            S_1, S_2, P_1, P_2, 
+                            k, w, lambda_s, c, tau, d)
+
+instance.draw_graph()
+print(instance)
+model = GRSC_CB_Model(instance, C=True, seed=SEED, output_flag=True)
+
+profiler = cProfile.Profile()
+profiler.enable()
+result = model.solve(verbose=True)
+profiler.disable()
+
+model.print_solution()
+
+stats = pstats.Stats(profiler)
+stats.sort_stats("cumtime")
+stats.print_stats(20)
